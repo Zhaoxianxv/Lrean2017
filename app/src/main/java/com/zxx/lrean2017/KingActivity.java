f8fbaa0f098d782c42c3d84cbc76a7a1b9d117f2
Administrator <1006584058@qq.com>
2019-11-23 09:49:20 +0800

    1.1

--- app/src/main/java/com/zxx/lrean2017/KingActivity.java
+++ app/src/main/java/com/zxx/lrean2017/KingActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -41,6 +42,7 @@ public class KingActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_king);
+        Log.e(TagFinal.ZXX,"测试个i太湖边");
     }
 
 
